098306e3c13529c83eed60c095dec60b61fc6dff
seaofnight <seaofnight00@gmail.com>
2019-06-26 11:29:21 +0900

    add skaffold test

--- 04.doing/01.skaffold/03.execute_test_application.sh
+++ 04.doing/01.skaffold/03.execute_test_application.sh
@@ -2,4 +2,15 @@
 
 cd skaffold/examples/getting-started
 
-skaffold dev --default-repo seaofnight/skaffold-example
+## docker auth config 
+# mkdir -p ~/.docker
+#cat << EOF >> ~/.docker/config.json
+#{
+#  "auths":{}
+#}
+#EOF
+# docker login required 
+# docker login --username yjkim1@ntels.com --password {passwordstr}
+
+
+skaffold dev --default-repo seaofnight
